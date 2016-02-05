bde37928e70e32b82fa0f896b987593dd65a0314
karthikbadam <sriram.karthik6@gmail.com>
2016-02-05 02:58:28 -0500

    barymap

---
+++ app.js
@@ -0,0 +1,350 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var csv = require('fast-csv');
+var fs = require('fs');
+var d3 = require('d3');
+var url = require('url');
+var qs = require('qs');
+
+var parseDate = d3.time.format("%d-%b-%y").parse;
+var parseYear = d3.time.format("%Y").parse;
+
+//var citiesToLoc = require('./maps.js').citiesToLoc;
+
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
+
+// connecting to Mongodb database running instance
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+
+var FIRST_TIME_EXECUTED = false;
+
+// connect to the flights database in mongodb
+//var mongourl = 'mongodb://127.0.0.1:27017/flights';
+var mongourl = 'mongodb://127.0.0.1:27017/movies';
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// Change from Default: html rendering engine 
+app.engine('html', require('ejs').renderFile);
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Change from Default:  Instead of routes and users 
+app.get('/visualization', function (req, res, next) {
+    res.render('largedisplay.html', {});
+});
+
+app.get('/awareness', function (req, res, next) {
+    res.render('awareness.html', {});
+});
+
+app.get('/mobile2', function (req, res, next) {
+    res.render('mobile2.html', {});
+});
+
+app.get('/mobile3', function (req, res, next) {
+    res.render('mobile3.html', {});
+});
+
+app.get('/study', function (req, res, next) {
+    res.render('data.html', {});
+});
+
+var gross = "Worldwide_Gross";
+var ratings = "IMDB_Rating";
+var budget = "Production_Budget";
+var date = "Release_Date";
+var director = "Director";
+var genre = "Major_Genre";
+var sales = "US_DVD_Sales";
+var runningTime = "Running_Time_min";
+var tomatoRating = "Rotten_Tomatoes_Rating";
+var imdbvotes = "IMDB_Votes";
+
+function initialize(db, callback) {
+    if (FIRST_TIME_EXECUTED) {
+
+        var obj;
+        fs.readFile("public/data/movies.json", 'utf8', function (err, data) {
+            if (err) throw err;
+            obj = JSON.parse(data);
+
+            for (var i = 0; i < obj.length; i++) {
+                var d = obj[i];
+
+                var temp = {};
+                temp[gross] = +d[gross];
+                temp[ratings] = +d[ratings];
+                temp[budget] = +d[budget];
+                if (("" + d[date]).split("-").length == 3) {
+                    temp[date] = "" + parseDate(d[date]);
+                } else {
+                    temp[date] = "" + parseYear(("" + d[date]));
+                }
+                temp[director] = d[director];
+                temp[genre] = d[genre];
+                temp[sales] = d[sales];
+                temp[runningTime] = d[runningTime];
+                temp[tomatoRating] = d[tomatoRating];
+                temp[imdbvotes] = d[imdbvotes];
+                
+                console.log(temp);
+
+                //add to database
+                db.collection('movies')
+                    .insertOne(temp,
+                        function (err, result) {
+                            assert.equal(err, null);
+                        });
+            }
+        });
+
+    }
+}
+
+
+// get all data based on a query of specific dimensions
+function queryMovies(db, query, callback) {
+
+    if (query != 0) {
+
+        var data = db.collection("movies")
+            .aggregate([
+                {
+                    $match: query
+        },
+                {
+                    $group: {
+                        "_id": {
+                            Worldwide_Gross: "$Worldwide_Gross",
+                            IMDB_Rating: "$IMDB_Rating",
+                            Production_Budget: "$Production_Budget",
+                            Release_Date: "$Release_Date",
+                            Director: "$Director",
+                            Major_Genre: "$Major_Genre",
+                            US_DVD_Sales: "$US_DVD_Sales",
+                            Running_Time_min: "$Running_Time_min", 
+                            IMDB_Votes: "$IMDB_Votes",
+                            Rotten_Tomatoes_Rating: "$Rotten_Tomatoes_Rating"
+                        },
+                        "IMDB_Rating": {
+                            $sum: "$IMDB_Rating"
+                        }
+                    }
+        }, {
+                    $sort: {
+                        "IMDB_Rating": -1
+                    }
+        }
+            ]);
+
+    } else {
+
+
+        var data = db.collection("movies")
+            .aggregate([
+                {
+                    $group: {
+                        "_id": {
+                            Worldwide_Gross: "$Worldwide_Gross",
+                            IMDB_Rating: "$IMDB_Rating",
+                            Production_Budget: "$Production_Budget",
+                            Release_Date: "$Release_Date",
+                            Director: "$Director",
+                            Major_Genre: "$Major_Genre",
+                            US_DVD_Sales: "$US_DVD_Sales",
+                            Running_Time_min: "$Running_Time_min", 
+                            IMDB_Votes: "$IMDB_Votes",
+                            Rotten_Tomatoes_Rating: "$Rotten_Tomatoes_Rating"
+                        },
+                        "IMDB_Rating": {
+                            $sum: "$IMDB_Rating"
+                        }
+                    }
+                }, {
+                    $sort: {
+                        "IMDB_Rating": -1
+                    }
+                }
+            ]);
+    }
+
+
+
+    data.toArray(function (err, docs) {
+        console.log(docs.length);
+        callback(docs);
+    });
+
+}
+
+app.get('/getMovies', function (req, res, next) {
+
+    var params = url.parse(req.url, true).query;
+
+    var query = parseQueryString(params);
+
+    MongoClient.connect(mongourl, function (err, db) {
+        assert.equal(null, err);
+
+        queryMovies(db, query,
+            function (data) {
+                db.close();
+                res.write(JSON.stringify(data));
+                res.end();
+            });
+    });
+
+});
+
+
+MongoClient.connect(mongourl, function (err, db) {
+    assert.equal(null, err);
+    initialize(db, function () {
+        db.close();
+    });
+});
+
+// parse query string
+function parseQueryString(params) {
+
+    var data = qs.parse(params).data;
+
+    console.log(JSON.stringify(data));
+
+    if (data == "empty") {
+        return 0;
+    }
+
+    var query = {};
+
+    for (var i = 0; i < data.length; i++) {
+
+        var q = {};
+
+        var d = data[i];
+
+        switch (d.operator) {
+
+        case "range":
+            if (d.index == "Date") {
+                q[d.index] = {
+                    "$gte": d.value[0],
+                    "$lte": d.value[1]
+                };
+            } else {
+                q[d.index] = {
+                    "$gte": parseFloat(d.value[0]),
+                    "$lte": parseFloat(d.value[1])
+                };
+            }
+            break;
+
+        case "equal":
+            q[d.index] = d.value;
+            break;
+
+        case "in":
+            for (var i = 0; i < d.value.length; i++) {
+                d.value[i] = parseFloat(d.value[i]);   
+            }
+            q[d.index] = {
+                "$in": d.value
+            };
+            break;
+
+        default:
+            console.log("Sorry, we are out of " + d.operator + ".");
+        }
+
+
+        switch (d.logic) {
+
+        case "AND":
+            query[d.index] = q[d.index];
+            break;
+
+        case "OR":
+            if (!query["$or"]) {
+                query["$or"] = [];
+            }
+            query["$or"].push(q);
+            break;
+
+        case "NOT":
+            query[d.index] = {
+                "$not": q[d.index]
+            };
+            break;
+
+        case "CLEAN":
+            query = {};
+            query[d.index] = q[d.index];
+            break;
+
+        default:
+            console.log("Sorry, we are out of " + d.logic + ".");
+        }
+
+    }
+    console.log(query);
+
+    return query;
+
+}
+
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
---
+++ public/js/ga-barymap.js
@@ -0,0 +1,294 @@
+var gross = "Worldwide_Gross";
+var ratings = "IMDB_Rating";
+var budget = "Production_Budget";
+var date = "Release_Date";
+var director = "Director";
+var genre = "Major_Genre";
+var sales = "US_DVD_Sales";
+var runningTime = "Running_Time_min";
+var tomatoRating = "Rotten_Tomatoes_Rating";
+var imdbvotes = "IMDB_Votes";
+
+var baryVertices = [gross, budget, imdbvotes, tomatoRating];
+
+var width = 0;
+
+var height = 0;
+
+var parseDate = d3.time.format("%d-%b-%y").parse;
+
+var month_names_short = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+var baryMap;
+
+var interactions = [{
+    query: [{
+        index: budget,
+        value: [60000000, 320000000],
+        operator: "range",
+        logic: "CLEAN"
+    }, {
+        index: gross,
+        value: [2000000000, 3000000000],
+        operator: "range",
+        logic: "AND"
+
+    }]
+}, {
+    query: [{
+        index: ratings,
+        value: [8, 10],
+        operator: "range",
+        logic: "CLEAN"
+    }]
+}, {
+    query: [{
+        index: ratings,
+        value: [0, 4],
+        operator: "range",
+        logic: "CLEAN"
+    }]
+}, {
+    query: [{
+        index: budget,
+        value: [10000000, 20000000],
+        operator: "range",
+        logic: "CLEAN"
+    }]
+}, {
+    query: [{
+        index: ratings,
+        value: [2, 5, 10],
+        operator: "in",
+        logic: "CLEAN"
+    }]
+}, {
+    query: [{
+        index: budget,
+        value: [0, 200000000],
+        operator: "range",
+        logic: "CLEAN"
+    }]
+}];
+
+$(document).ready(function () {
+
+    //creating the layout
+    width = $("#content").width();
+    height = $("#content").height();
+
+    var query = {
+        index1: budget,
+        operator1: "all",
+        value: "",
+        logic: "CLEAN",
+        index2: gross,
+        operator2: "all",
+        value: "",
+    };
+
+    createVisualizationfromQueryList([query]);
+
+});
+
+function createVisualizationfromQueryList(queryList) {
+
+    $.ajax({
+
+        type: "GET",
+        url: "/getMovies",
+        data: {
+            data: queryList
+        }
+
+    }).done(function (data) {
+
+        data = JSON.parse(data);
+
+        console.log(data);
+
+        baryMap = new BaryMap({
+            data: data,
+            cols: baryVertices
+        });
+
+
+        baryMap.createMap();
+
+    });
+
+}
+
+function BaryMap(options) {
+
+    var _self = this;
+
+    _self.data = options.data;
+
+    _self.cols = options.cols;
+
+    _self.dataMin = {};
+
+    _self.dataMax = {};
+
+    for (var i = 0; i < _self.data.length; i++) {
+
+        for (var j= 0; j < _self.cols.length; j++) {
+            
+            var key = _self.cols[j];
+
+            if (key in _self.dataMin) {
+                if (_self.dataMin[key] > _self.data[i]["_id"][key] && _self.data[i]["_id"][key] != null) {
+                    _self.dataMin[key] = _self.data[i]["_id"][key];
+                }
+            } else {
+                _self.dataMin[key] = _self.data[i]["_id"][key]? _self.data[i]["_id"][key]:Infinity;
+            }
+            
+            if (key in _self.dataMax) {
+                if (_self.dataMax[key] < _self.data[i]["_id"][key]) {
+                    _self.dataMax[key] = _self.data[i]["_id"][key];
+                }
+            } else {
+                _self.dataMax[key] = _self.data[i]["_id"][key]? _self.data[i]["_id"][key]:-Infinity;
+            }
+
+        }
+    }
+    
+    _self.dataTrans = new Array(_self.data.length); 
+    
+    for (var i = 0; i < _self.data.length; i++) {
+
+        var sum = 0; 
+        
+        _self.dataTrans[i] = {};
+        
+        for (var j= 0; j < _self.cols.length; j++) {
+            
+            var key = _self.cols[j];
+            
+            if (_self.data[i]["_id"][key] == null)
+                _self.data[i]["_id"][key] = _self.dataMin[key];
+            
+            _self.dataTrans[i][key] = (_self.data[i]["_id"][key] - _self.dataMin[key])/(_self.dataMax[key] - _self.dataMin[key]);
+            
+            sum += _self.dataTrans[i][key];
+
+        }
+        
+        for (var j= 0; j < _self.cols.length; j++) {
+            
+            var key = _self.cols[j];
+            
+            _self.dataTrans[i][key] = _self.dataTrans[i][key]/sum;
+
+        }
+        
+    }
+    
+    console.log(_self.dataTrans);
+}
+
+BaryMap.prototype.createMap = function () {
+
+    var _self = this;
+
+    var sides = _self.cols.length; // polygon vertices number
+    var radius = width / 4 - 50; // polygon radius
+
+    var svg = _self.svg = d3.select('#content')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('viewBox', [-width / 2, -height / 2, width, height].join(' '));
+
+    _self.container = svg.append('g');
+
+    _self.vertices = polygon(0, 0, radius, sides);
+    
+    //transform data into x, y
+     
+    for (var i = 0; i < _self.dataTrans.length; i++) {
+
+        _self.dataTrans[i].x = 0;
+        _self.dataTrans[i].y = 0;
+        
+        for (var j= 0; j < _self.cols.length; j++) {
+            
+            var key = _self.cols[j];
+            _self.dataTrans[i].x += _self.dataTrans[i][key]*_self.vertices[j][0];
+            _self.dataTrans[i].y += _self.dataTrans[i][key]*_self.vertices[j][1];
+            
+        }
+        
+    }
+
+    for (var i = 0; i < _self.vertices.length; i++) {
+
+        _self.container.append('line')
+            .attr('x1', _self.vertices[i][0])
+            .attr('y1', _self.vertices[i][1])
+            .attr('x2', _self.vertices[(i + 1) % _self.cols.length][0])
+            .attr('y2', _self.vertices[(i + 1) % _self.cols.length][1])
+            .attr('stroke', '#222')
+            .attr('stroke-width', "2px")
+            .attr('stroke-opacity', 0.7);
+
+    }
+
+    for (var i = 0; i < _self.vertices.length; i++) {
+
+        _self.container.append('circle')
+            .attr('r', 8)
+            .attr('cx', _self.vertices[i][0])
+            .attr('cy', _self.vertices[i][1])
+            .attr('fill', '#9ecae1');
+
+        _self.container.append('text')
+            .attr('x', function () {
+                if (i >= _self.vertices.length / 2) {
+                    return _self.vertices[i][0] - 10;
+                } else {
+                    return _self.vertices[i][0] + 10;
+                }
+            })
+            .attr('y', _self.vertices[i][1])
+            .style('text-anchor', function () {
+                if (i >= _self.vertices.length / 2) {
+                    return "end";
+                } else {
+                    return "start";
+                }
+            })
+            .attr('fill', '#222')
+            .text(_self.cols[i]);
+
+    }
+    
+     for (var i = 0; i < _self.dataTrans.length; i++) {
+
+        _self.container.append('circle')
+            .attr('r', 3)
+            .attr('cx', _self.dataTrans[i].x)
+            .attr('cy', _self.dataTrans[i].y)
+            .attr('fill', '#666')
+            .attr('fill-opacity', 0.5);
+     }
+
+    /* GIVEN x and y, the radius and the number of polygon sides RETURNS AN ARRAY OF VERTICE COORDINATES */
+    function polygon(x, y, radius, sides) {
+        var crd = [];
+
+        /* 1 SIDE CASE */
+        if (sides == 1)
+            return [[x, y]];
+
+        /* > 1 SIDE CASEs */
+        for (var i = 0; i < sides; i++) {
+            crd.push([(x + (Math.sin(2 * Math.PI * i / sides) * radius)), (y - (Math.cos(2 * Math.PI * i / sides) * radius))]);
+        }
+        return crd;
+    }
+
+}
