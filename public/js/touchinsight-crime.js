f7232c15fba8160b10004dd9da3dd42fde346acc
karthikbadam <sriram.karthik6@gmail.com>
2016-02-17 20:24:31 -0500

    crime

--- public/js/touchinsight-crime.js
+++ public/js/touchinsight-crime.js
@@ -13,13 +13,15 @@ crimeMeta["neighborhood"] = "Neighborhood";
 crimeMeta["lat"] = "Latitude";
 crimeMeta["lon"] = "Longitude";
 
-var numViews = 9; 
+var numViews = 9;
 
 var visuals = [
-    ['Description'], 
-    ['Weapon'], 
+    ['Description'],
+    ['Weapon'],
     ['Neighborhood'],
-    ['CrimeDate'],
+    ['District'],
+    ['Post'],
+    ['CrimeCode'],
 ];
 
 var index = 0;
@@ -55,21 +57,21 @@ $(document).ready(function () {
     width = $("#content").width();
     height = $("#content").height();
 
-    
+
     var gridster = $(".gridster").gridster({
         widget_margins: [5, 5],
-        min_cols: 3, 
-        autogrow_cols: true, 
+        min_cols: 3,
+        autogrow_cols: true,
         resize: {
             enabled: true
         },
         draggable: {
             handle: 'header'
         },
-        widget_base_dimensions: [width/3 - 10, height/3 - 10],
+        widget_base_dimensions: [width / 3 - 10, height / 3 - 10],
         autogenerate_stylesheet: true
     }).data('gridster');
-    
+
     for (var i = 0; i < numViews; i++) {
         gridster.add_widget('<div id = "viz' + i + '" ' + 'class="panel"><header></header></div>', 1, 1);
     }
@@ -85,7 +87,7 @@ $(document).ready(function () {
     };
 
     getDatafromQuery("empty");
-    
+
 });
 
 
@@ -118,9 +120,63 @@ function getDatafromQuery(queryList) {
 
         console.log(data);
 
-    
+        visuals.forEach(function (d, i) {
+
+            if (d.length == 1) {
+
+                var processed = processData(data, d[0]);
+                var visualization = new Bar({
+                    parentId: "viz"+i,
+                    cols: [d[0], "count"],
+                    width: $("#viz"+i).width(),
+                    height: $("#viz"+i).height(),
+                    text: "Count by " + d[0]
+                });
+
+                
+                visualization.updateVisualization(processed);
+
+            } else {
+
+            }
+        });
+
     });
 
+}
+
+function processData(data, col1, col2) {
+
+    var newData = {};
+
+    data.forEach(function (d) {
+
+        if (d["_id"][col1] in newData) {
+            var key = d["_id"][col1];
+            //count -- can be automated!!!
+            newData[key]++;
+            
+        } else {
+
+            var key = d["_id"][col1];
+            newData[key] = 1;
+        }
+    });
+
+    var returnData = [];
+
+    Object.keys(newData).forEach(function (k) {
+
+        var datum = {};
+        datum[col1] = k;
+        datum["count"] = newData[k];
+        returnData.push(datum);
+
+    });
+
+
+    console.log(returnData);
+    return returnData;
 }
 
 
@@ -129,217 +185,4 @@ function average(arr) {
     return arr.reduce(function (memo, num) {
         return memo + num;
     }, 0) / arr.length;
-}
-
-
-function processByYear(data) {
-
-    var newData = {};
-
-    data.forEach(function (d) {
-
-        var cdate = new Date(d["_id"][date]);
-        var cyear = cdate.getFullYear();
-        var cmonth = month_names_short[cdate.getMonth()];
-
-        if (cyear > 2011) {
-            cyear = cyear - 100;
-        }
-
-        //cdate = cmonth + "/" + cyear;
-        cdate = "" + cyear;
-
-        if (cdate in newData) {
-            newData[cdate][gross].push(d["_id"][gross]);
-            newData[cdate][budget].push(d["_id"][budget]);
-
-        } else {
-
-            newData[cdate] = {};
-            newData[cdate][gross] = [];
-            newData[cdate][budget] = [];
-            newData[cdate][gross].push(d["_id"][gross]);
-            newData[cdate][budget].push(d["_id"][budget]);
-        }
-    });
-
-    var returnData = [];
-
-    Object.keys(newData).forEach(function (k) {
-
-        var datum = {};
-        datum[date] = k;
-
-        if (k == "undefined/NaN" || k == "NaN")
-            return;
-
-        if (newData[k][gross].length == 0 || newData[k][budget].length == 0) {
-            delete newData[k];
-            return;
-        }
-
-        var avgGross = average(newData[k][gross]);
-        var avgBudget = average(newData[k][budget]);
-
-        datum["avg_" + gross] = avgGross;
-        datum["avg_" + budget] = avgBudget;
-        datum['dist_' + gross] = newData[k][gross];
-        datum['dist_' + budget] = newData[k][budget];
-
-        returnData.push(datum);
-
-    });
-
-    if (returnData.length == 1) {
-        var newDatum = {};
-
-        newDatum[date] = "" + (+returnData[0][date] - 1);
-        newDatum["avg_" + gross] = 0;
-        newDatum["avg_" + budget] = 0;
-
-        returnData.push(newDatum);
-    }
-
-    console.log(returnData);
-    return returnData;
-
-}
-
-function processByGenre(data) {
-
-    var newData = {};
-
-    data.forEach(function (d) {
-
-        if (d["_id"][genre] in newData) {
-            newData[d["_id"][genre]][gross].push(d["_id"][gross]);
-            newData[d["_id"][genre]][budget].push(d["_id"][budget]);
-
-        } else {
-
-            newData[d["_id"][genre]] = {};
-            newData[d["_id"][genre]][gross] = [];
-            newData[d["_id"][genre]][budget] = [];
-            newData[d["_id"][genre]][gross].push(d["_id"][gross]);
-            newData[d["_id"][genre]][budget].push(d["_id"][budget]);
-        }
-    });
-
-    var returnData = [];
-
-    Object.keys(newData).forEach(function (k) {
-
-        var datum = {};
-        datum[genre] = k;
-
-        if (newData[k][gross].length == 0 || newData[k][budget].length == 0) {
-            delete newData[k];
-            return;
-        }
-
-        var avgGross = average(newData[k][gross]);
-        var avgBudget = average(newData[k][budget]);
-
-        datum["avg_" + gross] = avgGross;
-        datum["avg_" + budget] = avgBudget;
-        datum['dist_' + gross] = newData[k][gross];
-        datum['dist_' + budget] = newData[k][budget];
-
-        returnData.push(datum);
-
-    });
-
-
-    console.log(returnData);
-    return returnData;
-}
-
-function createLayout() {
-
-    top = d3.select("#content").append("div")
-        .attr("id", "topDiv")
-        .attr("class", "panel")
-        .style("width", 2 * width / 3)
-        .style("height", height / 4 - 2)
-        .style("background-color", "white")
-        .style("overflow", "hidden")
-        .style("margin-left", width / 6);
-
-    left = d3.select("#content").append("div")
-        .attr("id", "leftDiv")
-        .attr("class", "panel")
-        .style("width", width / 3 - 2)
-        .style("height", height / 2 - 2)
-        .style("background-color", "white")
-        .style("overflow", "hidden");
-
-    main = d3.select("#content").append("div")
-        .attr("id", "mainDiv")
-        .attr("class", "panel")
-        .style("width", width / 3 - 2)
-        .style("height", height / 2 - 2)
-        .style("background-color", "white")
-        .style("overflow", "hidden");
-
-    right = d3.select("#content").append("div")
-        .attr("id", "rightDiv")
-        .attr("class", "panel")
-        .style("width", width / 3 - 2)
-        .style("height", height / 2 - 2)
-        .style("background-color", "white")
-        .style("overflow", "hidden");
-
-    bottom = d3.select("#content").append("div")
-        .attr("id", "bottomDiv")
-        .attr("class", "panel")
-        .style("width", 2 * width / 3)
-        .style("height", height / 4 - 2)
-        .style("background-color", "white")
-        .style("overflow", "hidden")
-        .style("margin-left", width / 6);
-
-}
-
-function onDataLoaded() {
-
-    //creating the views
-    gross_time = new TimeChart({
-        parentId: "topDiv",
-        cols: [date, "avg_" + gross],
-        width: $("#topDiv").width(),
-        height: $("#topDiv").height(),
-        text: "Avg. Gross by Time"
-    });
-
-    genre_gross = new Bar({
-        parentId: "leftDiv",
-        cols: [genre, "avg_" + gross],
-        width: $("#leftDiv").width(),
-        height: $("#leftDiv").height(),
-        text: "Avg. Gross by Genre"
-    });
-
-    gross_budget = new ScatterPlot({
-        parentId: "mainDiv",
-        cols: [budget, gross],
-        width: $("#mainDiv").width(),
-        height: $("#mainDiv").height(),
-    });
-
-    genre_budget = new Bar({
-        parentId: "rightDiv",
-        cols: [genre, "avg_" + budget],
-        width: $("#rightDiv").width(),
-        height: $("#rightDiv").height(),
-        text: "Avg. Budget by Genre"
-    });
-
-    budget_time = new TimeChart({
-        parentId: "bottomDiv",
-        cols: [date, "avg_" + budget],
-        width: $("#bottomDiv").width(),
-        height: $("#bottomDiv").height(),
-        text: "Avg. Budget by Time"
-    });
-
 }
